f6f01514080dee3eefb130194b4bad2f4a56e39a
Toby <toby.tripp+git@gmail.com>
2010-03-10 13:26:15 -0600

    Do not reference RAILS_ENV either, if it is undefined

--- lib/git-precommit/tasks.rb
+++ lib/git-precommit/tasks.rb
@@ -1,7 +1,8 @@
 if defined? Rails
   ENVIRONMENT = Rails.env
 else 
-  ENVIRONMENT = RAILS_ENV || "development"
+  ENVIRONMENT   = RAILS_ENV if defined? RAILS_ENV
+  ENVIRONMENT ||= "development"
 end
 
 if %w[development test].include? ENVIRONMENT
